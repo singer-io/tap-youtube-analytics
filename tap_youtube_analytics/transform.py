f96f18bf479661cae9ca87a549a45c9b471f3df4
pbegle <peter.begle@bytecode.io>
2021-03-09 12:04:59 -0800

    Removing dimension mapping logic that altered the raw data.

--- tap_youtube_analytics/transform.py
+++ tap_youtube_analytics/transform.py
@@ -38,26 +38,12 @@ def transform_report_record(record, dimensions, report):
 
     new_record = record.copy()
 
-    map_path = get_abs_path('dim_lookup_map.json')
-    with open(map_path) as file:
-        dim_lookup_map = json.load(file)
-
     dimension_values = {}
 
     for key, val in list(record.items()):
         # Add dimension key-val to dimension_values dictionary
         if key in dimensions:
             dimension_values[key] = val
-
-        # Transform dim values from codes to names using dim_lookup_map
-        if key in dim_lookup_map:
-            # lookup new_val, with a default for existing val (if not found)
-            new_val = dim_lookup_map[key].get(val, val)
-            if val == new_val:
-                LOGGER.warning('dim_lookup_map value not found; key: {}, value: {}'.format(key, val))
-            new_record[key] = new_val
-        else:
-            new_record[key] = val
 
     # Add report fields to data
     new_record['report_id'] = report.get('id')
