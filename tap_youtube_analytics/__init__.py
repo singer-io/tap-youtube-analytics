542143faa80ea8d8e998b04f801fe4296318a6e1
ahurkat <aastha.hurkat@qlik.com>
2025-10-20 09:12:08 -0700

    Update tap_youtube_analytics/__init__.py
    Co-authored-by: Rushikesh Todkar <98420315+RushiT0122@users.noreply.github.com>

--- tap_youtube_analytics/__init__.py
+++ tap_youtube_analytics/__init__.py
@@ -30,10 +30,10 @@ def main():
             do_discover()
         elif parsed_args.catalog:
             sync(
-                client=client,
-                config=parsed_args.config,
-                catalog=parsed_args.catalog,
-                state=state)
+                 client=client,
+                 config=parsed_args.config,
+                 catalog=parsed_args.catalog,
+                 state=state)
 
 
 if __name__ == "__main__":
