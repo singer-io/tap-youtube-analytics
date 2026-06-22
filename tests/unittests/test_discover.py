fdfa1cf07aeeb3fee91f660d9b34d9ed01f45cea
rsaha-qlik <vpz@qlik.com>
2026-06-22 11:19:24 +0530

    updated unit tests

--- tests/unittests/test_discover.py
+++ tests/unittests/test_discover.py
@@ -316,7 +316,7 @@ class TestDiscover(unittest.TestCase):
 
         with self.assertRaises(YoutubeAnalyticsNoAccessibleStreamsError) as ctx:
             discover(MagicMock())
-        self.assertIn("No stream endpoints are accessible", str(ctx.exception))
+        self.assertIn("'read' access to any supported streams", str(ctx.exception))
 
     @patch("tap_youtube_analytics.discover._check_reporting_api_access")
     @patch("tap_youtube_analytics.discover._check_data_api_access")
